0184eecf8db146610fecb2586dce1df2affd84c0
fischer23 <88708145+fischer23@users.noreply.github.com>
2022-11-02 08:19:59 +0100

    Add files via upload

---
+++ Plot_closedADDIS_consP_noAlpha.R
@@ -0,0 +1,162 @@
+#Generates Figure 5 of the paper "The Online Closure Principle"
+
+rm(list=ls())
+library(ggplot2)
+library(MASS)
+library(patchwork)
+###load the procedures
+source("Procedures.R")
+
+###Gaussian testing problem for exact p-values
+m=2000   #Number of Trials
+n=1000    #Number of Hypotheses per Trial
+mu_A=4    #Strength of the alternative
+mu_N=-2   #Conservativeness of null p-values (<0 for conservative null p-values)
+#pi_A is defined in the loop below
+
+###Initialise Hyperparameters
+seq_1_n=seq(1,n,1)
+gamma=6/(pi^2*(seq_1_n^2))
+alpha=0.2
+tau=0.8
+lambda=0.3
+
+#Correlation within batch
+corr=0.8
+
+###Set seed to make the results reproducible
+set.seed(12345)
+
+#Set the different batch sizes
+batch_sizes=c(1,10,25,100)
+
+###Predefine vectors for FWER and power of the different procedures
+FWER_Spending=matrix(0,9,length(batch_sizes))
+power_Spending=matrix(0,9,length(batch_sizes))
+FWER_cSpending=matrix(0,9,length(batch_sizes))
+power_cSpending=matrix(0,9,length(batch_sizes))
+b=1 #Counter 
+for(batch_size in batch_sizes){
+###Parameters for a local dependence structure given by batches
+batch_number=ceiling(n/batch_size)
+lags=rep(seq(0,(batch_size-1),1),batch_number)
+lags=lags[1:n]
+sigma=matrix(corr,batch_size,batch_size)+diag((1-corr),batch_size)
+mu=rep(0,batch_size)
+
+###Generate p-values and compute FWER and power for the desired procedures
+for(l in 1:9){
+  ##Fast way for p-values in batches.
+  pi_A=l/10
+  p=matrix(,nrow=n,ncol=m)
+  hypo=matrix(,nrow=n,ncol=m)
+  for(j in 1:m){
+    hypo[,j]=rbinom(n,1,pi_A)
+    X=rep(0,batch_number*batch_size)
+    for(k in 1:batch_number){
+    X[((k-1)*batch_size+1):(k*batch_size)]=mvrnorm(1,mu,sigma)
+    }
+    X=X[1:n]
+    Z=mu_N*(hypo[,j]-1)*(-1)+mu_A*hypo[,j]+X
+    p[,j]=pnorm(-Z)
+  }
+
+##ADDIS-Spending
+V=rep(0,m)      #Indicates, whether there was at least one type 1 error in a trial
+power=rep(0,m)  #Power within each trial
+
+for(j in 1:m){
+  alpha_ind=ADDIS_Spending(alpha, gamma, tau, lambda,lags, p[,j], n)
+  hypo_est=alpha_ind>=p[,j]
+  V[j]=max((hypo[,j]==0 & hypo_est==1))
+  D=(hypo[,j]==1 & hypo[,j]==hypo_est)
+  power[j]=sum(D)/sum(hypo[,j])
+}
+FWER_Spending[l,b]=mean(V)
+power_Spending[l,b]=mean(power)
+
+##Closed ADDIS-Spending
+V=rep(0,m)      #Indicates, whether there was atleast one type 1 error in a trial
+power=rep(0,m)  #Power within each trial
+
+for(j in 1:m){
+  alpha_ind=closed_ADDIS_Spending(alpha, gamma, tau,lambda,lags, p[,j], n)
+  hypo_est=alpha_ind>=p[,j]
+  V[j]=max((hypo[,j]==0 & hypo_est==1))
+  D=(hypo[,j]==1 & hypo[,j]==hypo_est)
+  power[j]=sum(D)/sum(hypo[,j])
+}
+FWER_cSpending[l,b]=mean(V)
+power_cSpending[l,b]=mean(power)
+
+}
+b=b+1
+}
+###Create Plot for ADDIS-Spending
+
+results_df=data.frame(seq(0.1,0.9,0.1),power_Spending,FWER_Spending)
+
+p_spending=ggplot(results_df, aes(seq.0.1..0.9..0.1.)) + 
+  geom_line(aes(y = X1, linetype = "1",colour = "2")) + 
+  geom_point(aes(y = X1,colour = "2")) +
+  geom_line(aes(y = X1.1, linetype = "1",colour = "2"))+
+  geom_point(aes(y = X1.1,colour = "2"))+
+  geom_line(aes(y = X2, linetype = "10",colour = "2")) + 
+  geom_point(aes(y = X2,colour = "2")) +
+  geom_line(aes(y = X2.1, linetype = "10",colour = "2"))+
+  geom_point(aes(y = X2.1,colour = "2"))+
+  geom_line(aes(y = X3, linetype = "25",colour = "2")) + 
+  geom_point(aes(y = X3,colour = "2")) +
+  geom_line(aes(y = X3.1, linetype = "25",colour = "2"))+
+  geom_point(aes(y = X3.1,colour = "2"))+
+  geom_line(aes(y = X4, linetype = "100",colour = "2")) + 
+  geom_point(aes(y = X4,colour = "2")) +
+  geom_line(aes(y = X4.1, linetype = "100",colour = "2"))+
+  geom_point(aes(y = X4.1,colour = "2"))+
+  geom_hline(yintercept=alpha)+
+  scale_colour_manual(guide="none", values=c("1"="limegreen", "2"="#f84f4f"))+
+  scale_linetype_manual(name  ="Batch-size", values = c("1"="solid","10"="longdash","25"="dashed","100"="dotted"))+
+  xlab(expression(pi[A]))+
+  ylab("FWER / Power")+
+  scale_x_continuous(breaks = seq(0.2,0.8,0.2), limits=c(0.1,0.9),expand = c(0, 0))+
+  scale_y_continuous(breaks = seq(0,1.2,0.2), limits=c(0,1),expand = c(0, 0))+
+  theme(panel.grid.major = element_line(color = "grey", size = 0.5,linetype = 1),
+        panel.border = element_rect(colour = "black", fill=NA, size=1))+
+  ggtitle("ADDIS-Spending") 
+
+###Create Plot for closed ADDIS-Spending
+results_df_cSpending=data.frame(seq(0.1,0.9,0.1), power_cSpending, FWER_cSpending)
+
+p_cSpending=ggplot(results_df_cSpending, aes(seq.0.1..0.9..0.1.)) + 
+  geom_line(aes(y = X1, linetype = "1",colour = "4")) + 
+  geom_point(aes(y = X1,colour = "4")) +
+  geom_line(aes(y = X1.1, linetype = "1",colour = "4"))+
+  geom_point(aes(y = X1.1),colour = "4")+
+  geom_line(aes(y = X2, linetype = "10",colour = "4")) + 
+  geom_point(aes(y = X2,colour = "4")) +
+  geom_line(aes(y = X2.1, linetype = "10",colour = "4"))+
+  geom_point(aes(y = X2.1,colour = "4"))+
+  geom_line(aes(y = X3, linetype = "25",colour = "4")) + 
+  geom_point(aes(y = X3,colour = "4")) +
+  geom_line(aes(y = X3.1, linetype = "25",colour = "4"))+
+  geom_point(aes(y = X3.1,colour = "4"))+
+  geom_line(aes(y = X4, linetype = "100",colour = "4")) + 
+  geom_point(aes(y = X4,colour = "4")) +
+  geom_line(aes(y = X4.1, linetype = "100",colour = "4"))+
+  geom_point(aes(y = X4.1,colour = "4"))+
+  geom_hline(yintercept=alpha)+
+  scale_colour_manual(guide="none", values=c("3"="limegreen", "4"="cornflowerblue"))+
+  scale_linetype_manual(name  ="Batch-size", values = c("1"="solid","10"="longdash","25"="dashed","100"="dotted"))+
+  xlab(expression(pi[A]))+
+  ylab("FWER / Power")+
+  scale_x_continuous(breaks = seq(0.2,0.8,0.2), limits=c(0.1,0.9),expand = c(0, 0))+
+  scale_y_continuous(breaks = seq(0,1.2,0.2), limits=c(0,1),expand = c(0, 0))+
+  theme(panel.grid.major = element_line(color = "grey", size = 0.5,linetype = 1),
+        panel.border = element_rect(colour = "black", fill=NA, size=1))+
+  ggtitle("Closed ADDIS-Spending")
+
+combined <- p_spending + p_cSpending  & theme(legend.position = "bottom")
+combined + plot_layout(guides = "collect")
+
+ggsave("Plot_closedADDIS_consP_noAlph.png")
+
---
+++ Procedures.R
@@ -0,0 +1,85 @@
+###This R-file contains the procedures that were used for the simulations in my master's thesis
+
+### We only need ADDIS-Spending under local dependence, ADDIS-Graph under local dependence, E-ADDIS-Spending, E-ADDIS-Graph, EI-ADDIS-Spending, closed Alpha-Spending, Online-Graph and Alpha-Spending.
+### All other procedures are special cases of these.
+
+#Input (at most): alpha, gamma, w, tau, lambda,lags, p, n. 
+
+#alpha: overall significance level. Number between 0 and 1
+#gamma: Weights for Alpha-Spending. Non-negative n-dimensional vector with sum less than 1.
+#w:     Weights for Graph procedures. Upper triangle n x n matrix with row sum less than 1
+#tau:   Used for discarding procedures. n-dimensional vector with values between 0 and 1. Besides, it can be chosen as a fixed number between 0 and 1.
+#lambda:Used for the adaptive procedures. n-dimensional vector with values between 0 and tau_i for adaptive procedures or tau_i*alpha^(i) and tau_i for exhaustive adaptive procedures. Besides, it can be chosen as a fixed number fulfilling these conditions.
+#lags:  Representing the given local dependence structure. n-dimensional vector of natural numbers (including 0) or a fix natural number (including 0).
+#p:     n-dimensional vector of p-values. 
+#n:     Number of hypotheses.
+
+#ADDIS-Spending under local dependence 
+ADDIS_Spending=function(alpha, gamma, tau, lambda,lags, p, n){
+  if(length(lags)== 1){lags=rep(lags,n)}
+  if(length(tau)== 1){tau=rep(tau,n)}
+  if(length(lambda)== 1){lambda=rep(lambda,n)}
+  if(length(gamma)!=n | length(tau)!= n | length(lambda)!=n| length(p)!=n| length(lags)!=n){
+    warning("mismatching length")
+  }
+  S_C=rep(0,n)
+  t=rep(1,n)
+  alpha_ind=rep(0,n)
+  for(i in 1:n){
+    if(lags[i]>=i-1){
+      t[i]=i
+    }else{
+      t[i]=1+lags[i]+sum(S_C[1:(i-lags[i]-1)])
+    }
+    alpha_ind[i]=alpha*gamma[t[i]]*(tau[i]-lambda[i])
+    if(p[i]<=tau[i] & p[i]>lambda[i]){
+      S_C[i]=1
+    }
+  }
+  return(alpha_ind)
+}
+
+#Closed ADDIS-Spending under local dependence 
+closed_ADDIS_Spending=function(alpha, gamma, tau, lambda,lags, p, n){
+  if(length(lags)== 1){lags=rep(lags,n)}
+  if(length(tau)== 1){tau=rep(tau,n)}
+  if(length(lambda)== 1){lambda=rep(lambda,n)}
+  if(length(gamma)!=n | length(tau)!= n | length(lambda)!=n| length(p)!=n| length(lags)!=n){
+    warning("mismatching length")
+  }
+  S_C=rep(0,n)
+  R=rep(0,n)
+  t=rep(1,n)
+  alpha_ind=rep(0,n)
+  for(i in 1:n){
+    if(lags[i]>=1){
+    if(lags[i]>=i-1){
+      t[i]=1+sum(1-(R[(i-lags[i]):(i-1)]))
+    }else{
+      t[i]=1+sum(S_C[1:(i-lags[i]-1)])+sum(1-(R[(i-lags[i]):(i-1)]))
+    }
+    }else{
+      t[i]=1+sum(S_C[1:(i-1)])
+    }
+    alpha_ind[i]=alpha*gamma[t[i]]*(tau[i]-lambda[i])
+    if(p[i]<=tau[i] & p[i]>lambda[i]){
+      S_C[i]=1
+    }
+    if(p[i]<=alpha_ind[i]){
+      R[i]=1
+    }
+  }
+  return(alpha_ind)
+}
+
+#Alpha-Spending
+alpha_spending=function(alpha, gamma, p, n){
+  if(length(gamma)!=n | length(p)!=n){
+    warning("mismatching length")
+  }
+  alpha_ind=rep(0,n)
+  for(i in 1:n){
+    alpha_ind[i]=alpha*gamma[i]
+  }
+  return(alpha_ind)
+} 
