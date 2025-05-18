5b60517ee440f62ebaf693e697262442a69c57cc
Lasse Fischer <fischer1@uni-bremen.de>
2025-05-18 11:43:47 +0200

    Improved formatting

--- OCP_Procedures.R
+++ OCP_Procedures.R
@@ -1,112 +1,124 @@
-###This R-file contains the procedures that were used for simulations in the paper "The Online Closure Principle"
+### This R-file contains the procedures that were used for simulations in the paper "The Online Closure Principle"
 
-#Input: alpha, gamma, tau, lambda,lags, p, n. 
+# Input: alpha, gamma, tau, lambda,lags, p, n.
 
-#alpha: Overall significance level. Number between 0 and 1.
-#gamma: Weights for Alpha-Spending. Non-negative n-dimensional vector with sum less or equal than 1.
-#w:     Edge weights used for Online-Graph. Non-negative n x n upper triangular matrix with row sum less or equal than 1.
-#tau:   Used for discarding procedures. n-dimensional vector with values between 0 and 1. Besides, it can be chosen as a fixed number between 0 and 1.
-#lambda:Used for the adaptive procedures. n-dimensional vector with values between 0 and tau_i. Besides, it can be chosen as a fixed number fulfilling these conditions.
-#lags:  Representing the given local dependence structure. n-dimensional vector of natural numbers (including 0) or a fix natural number (including 0).
-#p:     n-dimensional vector of p-values. 
-#n:     Number of hypotheses.
+# alpha: Overall significance level. Number between 0 and 1.
+# gamma: Weights for Alpha-Spending. Non-negative n-dimensional vector with sum less or equal than 1.
+# w:     Edge weights used for Online-Graph. Non-negative n x n upper triangular matrix with row sum less or equal than 1.
+# tau:   Used for discarding procedures. n-dimensional vector with values between 0 and 1. Besides, it can be chosen as a fixed number between 0 and 1.
+# lambda:Used for the adaptive procedures. n-dimensional vector with values between 0 and tau_i. Besides, it can be chosen as a fixed number fulfilling these conditions.
+# lags:  Representing the given local dependence structure. n-dimensional vector of natural numbers (including 0) or a fix natural number (including 0).
+# p:     n-dimensional vector of p-values.
+# n:     Number of hypotheses.
 
-#ADDIS-Spending under local dependence (Tian and Ramdas, 2021)
-ADDIS_Spending=function(alpha, gamma, tau, lambda,lags, p, n){
-  if(length(lags)== 1){lags=rep(lags,n)}
-  if(length(tau)== 1){tau=rep(tau,n)}
-  if(length(lambda)== 1){lambda=rep(lambda,n)}
-  if(length(gamma)!=n | length(tau)!= n | length(lambda)!=n| length(p)!=n| length(lags)!=n){
+# ADDIS-Spending under local dependence (Tian and Ramdas, 2021)
+ADDIS_Spending <- function(alpha, gamma, tau, lambda, lags, p, n) {
+  if (length(lags) == 1) {
+    lags <- rep(lags, n)
+  }
+  if (length(tau) == 1) {
+    tau <- rep(tau, n)
+  }
+  if (length(lambda) == 1) {
+    lambda <- rep(lambda, n)
+  }
+  if (length(gamma) != n | length(tau) != n | length(lambda) != n | length(p) != n | length(lags) != n) {
     warning("mismatching length")
   }
-  S_C=rep(0,n)
-  t=rep(1,n)
-  alpha_ind=rep(0,n)
-  for(i in 1:n){
-    if(lags[i]>=i-1){
-      t[i]=i
-    }else{
-      t[i]=1+lags[i]+sum(S_C[1:(i-lags[i]-1)])
+  S_C <- rep(0, n)
+  t <- rep(1, n)
+  alpha_ind <- rep(0, n)
+  for (i in 1:n) {
+    if (lags[i] >= i - 1) {
+      t[i] <- i
+    } else {
+      t[i] <- 1 + lags[i] + sum(S_C[1:(i - lags[i] - 1)])
     }
-    alpha_ind[i]=alpha*gamma[t[i]]*(tau[i]-lambda[i])
-    if(p[i]<=tau[i] & p[i]>lambda[i]){
-      S_C[i]=1
+    alpha_ind[i] <- alpha * gamma[t[i]] * (tau[i] - lambda[i])
+    if (p[i] <= tau[i] & p[i] > lambda[i]) {
+      S_C[i] <- 1
     }
   }
   return(alpha_ind)
 }
 
-#Closed ADDIS-Spending under local dependence 
-closed_ADDIS_Spending=function(alpha, gamma, tau, lambda,lags, p, n){
-  if(length(lags)== 1){lags=rep(lags,n)}
-  if(length(tau)== 1){tau=rep(tau,n)}
-  if(length(lambda)== 1){lambda=rep(lambda,n)}
-  if(length(gamma)!=n | length(tau)!= n | length(lambda)!=n| length(p)!=n| length(lags)!=n){
+# Closed ADDIS-Spending under local dependence
+closed_ADDIS_Spending <- function(alpha, gamma, tau, lambda, lags, p, n) {
+  if (length(lags) == 1) {
+    lags <- rep(lags, n)
+  }
+  if (length(tau) == 1) {
+    tau <- rep(tau, n)
+  }
+  if (length(lambda) == 1) {
+    lambda <- rep(lambda, n)
+  }
+  if (length(gamma) != n | length(tau) != n | length(lambda) != n | length(p) != n | length(lags) != n) {
     warning("mismatching length")
   }
-  S_C=rep(0,n)
-  R=rep(0,n)
-  t=rep(1,n)
-  alpha_ind=rep(0,n)
-  for(i in 1:n){
-    if(lags[i]>=1){
-    if(lags[i]>=i-1){
-      t[i]=1+sum(1-(R[(i-lags[i]):(i-1)]))
-    }else{
-      t[i]=1+sum(S_C[1:(i-lags[i]-1)])+sum(1-(R[(i-lags[i]):(i-1)]))
+  S_C <- rep(0, n)
+  R <- rep(0, n)
+  t <- rep(1, n)
+  alpha_ind <- rep(0, n)
+  for (i in 1:n) {
+    if (lags[i] >= 1) {
+      if (lags[i] >= i - 1) {
+        t[i] <- 1 + sum(1 - (R[(i - lags[i]):(i - 1)]))
+      } else {
+        t[i] <- 1 + sum(S_C[1:(i - lags[i] - 1)]) + sum(1 - (R[(i - lags[i]):(i - 1)]))
+      }
+    } else {
+      t[i] <- 1 + sum(S_C[1:(i - 1)])
     }
-    }else{
-      t[i]=1+sum(S_C[1:(i-1)])
+    alpha_ind[i] <- alpha * gamma[t[i]] * (tau[i] - lambda[i])
+    if (p[i] <= tau[i] & p[i] > lambda[i]) {
+      S_C[i] <- 1
     }
-    alpha_ind[i]=alpha*gamma[t[i]]*(tau[i]-lambda[i])
-    if(p[i]<=tau[i] & p[i]>lambda[i]){
-      S_C[i]=1
-    }
-    if(p[i]<=alpha_ind[i]){
-      R[i]=1
+    if (p[i] <= alpha_ind[i]) {
+      R[i] <- 1
     }
   }
   return(alpha_ind)
 }
 
 
-#Online Graph
-Online_Graph=function(alpha, gamma, w, p, n){
-  if(length(gamma)!=n | length(p)!=n){
+# Online Graph
+Online_Graph <- function(alpha, gamma, w, p, n) {
+  if (length(gamma) != n | length(p) != n) {
     warning("mismatching length")
   }
-  alpha_ind=rep(0,n)
-  alpha_ind[1]=alpha*gamma[1]
-  for(i in 2:n){
-    alpha_ind[i]=alpha*gamma[i]+sum((alpha_ind[1:(i-1)]>=p[1:(i-1)])*alpha_ind[1:(i-1)]*w[1:(i-1),i])
+  alpha_ind <- rep(0, n)
+  alpha_ind[1] <- alpha * gamma[1]
+  for (i in 2:n) {
+    alpha_ind[i] <- alpha * gamma[i] + sum((alpha_ind[1:(i - 1)] >= p[1:(i - 1)]) * alpha_ind[1:(i - 1)] * w[1:(i - 1), i])
   }
   return(alpha_ind)
 }
 
 
-#Alpha-Spending
-alpha_Spending=function(alpha, gamma, p, n){
-  if(length(gamma)!=n | length(p)!=n){
+# Alpha-Spending
+alpha_Spending <- function(alpha, gamma, p, n) {
+  if (length(gamma) != n | length(p) != n) {
     warning("mismatching length")
   }
-  alpha_ind=rep(0,n)
-  for(i in 1:n){
-    alpha_ind[i]=alpha*gamma[i]
+  alpha_ind <- rep(0, n)
+  for (i in 1:n) {
+    alpha_ind[i] <- alpha * gamma[i]
   }
   return(alpha_ind)
-} 
+}
 
-#closed Alpha-Spending
-closed_Alpha_Spending=function(alpha, gamma, p, n){
-  if(length(gamma)!=n | length(p)!=n){
+# closed Alpha-Spending
+closed_Alpha_Spending <- function(alpha, gamma, p, n) {
+  if (length(gamma) != n | length(p) != n) {
     warning("mismatching length")
   }
-  t=1
-  alpha_ind=rep(0,n)
-  for(i in 1:n){
-    alpha_ind[i]=alpha*gamma[t]
-    if(p[i]>alpha_ind[i]){
-      t=t+1
+  t <- 1
+  alpha_ind <- rep(0, n)
+  for (i in 1:n) {
+    alpha_ind[i] <- alpha * gamma[t]
+    if (p[i] > alpha_ind[i]) {
+      t <- t + 1
     }
   }
   return(alpha_ind)
--- Plot_platform_RECOVERY.R
+++ Plot_platform_RECOVERY.R
@@ -1,94 +1,92 @@
-#Used to calculate Table 2 in the paper "The Online Closure Principle"
+# Used to calculate Table 2 in the paper "The Online Closure Principle"
 
-rm(list=ls())
+rm(list = ls())
 library(ggplot2)
 library(MASS)
 library(patchwork)
 library(Matrix)
 library(mvtnorm)
 
-###load the procedures
+### load the procedures
 source("OCP_Procedures.R")
 
-#Test is not finished for Sotrovimab. It is only included to determine \alpha_{13}.
+# Test is not finished for Sotrovimab. It is only included to determine \alpha_{13}.
 
-names=c("Dexamethasone", "Lopinavir-ritonavir","Hydroxychloroquine", "Azithromycin", 
-        "Tocilizumab","Convalescent plasma", "Casirivimab-Imdevimab", "Aspirin",
-        "Colchicine", "Baricitinib", "High-dose steroids", "Empagliflozin", "Sotrovimab")
+names <- c(
+  "Dexamethasone", "Lopinavir-ritonavir", "Hydroxychloroquine", "Azithromycin",
+  "Tocilizumab", "Convalescent plasma", "Casirivimab-Imdevimab", "Aspirin",
+  "Colchicine", "Baricitinib", "High-dose steroids", "Empagliflozin", "Sotrovimab"
+)
 
-lags=c(0,1,2,3,4,5,3,3,3,3,1,2,2)
+lags <- c(0, 1, 2, 3, 4, 5, 3, 3, 3, 3, 1, 2, 2)
 
-p=c(0.0003,0.58,0.1,0.99,0.007,0.34,0.001,0.35,0.63,0.026,0.0012,0.64,1)
+p <- c(0.0003, 0.58, 0.1, 0.99, 0.007, 0.34, 0.001, 0.35, 0.63, 0.026, 0.0012, 0.64, 1)
 
-n=13
+n <- 13
 
-###We run the experiment for different gamma
-n_q=3
+### We run the experiment for different gamma
+n_q <- 3
 
-alpha_ind_Spending=matrix(0,ncol=n_q,nrow=n)
-rej_Spending=matrix(0,ncol=n_q,nrow=n)
-n_rej_Spending=rep(0,n_q)
+alpha_ind_Spending <- matrix(0, ncol = n_q, nrow = n)
+rej_Spending <- matrix(0, ncol = n_q, nrow = n)
+n_rej_Spending <- rep(0, n_q)
 
-alpha_ind_closed_ADDIS=matrix(0,ncol=n_q,nrow=n)
-rej_closed_ADDIS=matrix(0,ncol=n_q,nrow=n)
-n_rej_closed_ADDIS=rep(0,n_q)
+alpha_ind_closed_ADDIS <- matrix(0, ncol = n_q, nrow = n)
+rej_closed_ADDIS <- matrix(0, ncol = n_q, nrow = n)
+n_rej_closed_ADDIS <- rep(0, n_q)
 
-alpha_ind_Graph_online=matrix(0,ncol=n_q,nrow=n)
-rej_Graph_online=matrix(0,ncol=n_q,nrow=n)
-n_rej_Graph_online=rep(0,n_q)
+alpha_ind_Graph_online <- matrix(0, ncol = n_q, nrow = n)
+rej_Graph_online <- matrix(0, ncol = n_q, nrow = n)
+n_rej_Graph_online <- rep(0, n_q)
 
-alpha_ind_closed_Alpha=matrix(0,ncol=n_q,nrow=n)
-rej_closed_Alpha=matrix(0,ncol=n_q,nrow=n)
-n_rej_closed_Alpha=rep(0,n_q)
+alpha_ind_closed_Alpha <- matrix(0, ncol = n_q, nrow = n)
+rej_closed_Alpha <- matrix(0, ncol = n_q, nrow = n)
+n_rej_closed_Alpha <- rep(0, n_q)
 
-alpha_ind_Alpha=matrix(0,ncol=n_q,nrow=n)
-rej_Alpha=matrix(0,ncol=n_q,nrow=n)
-n_rej_Alpha=rep(0,n_q)
+alpha_ind_Alpha <- matrix(0, ncol = n_q, nrow = n)
+rej_Alpha <- matrix(0, ncol = n_q, nrow = n)
+n_rej_Alpha <- rep(0, n_q)
 
 
 
-count=1
+count <- 1
 
-for(q in c(0.6,0.7,0.8)){
-  
-  ###Initialise Hyperparameters
-  gamma=q^(1:n)*(1-q)/q
-  tau=0.8
-  lambda=0.16
-  w=abs(matrix(1:n-1 , nrow = n, ncol = n, byrow = TRUE) - (1:n-1))
-  w[w==0]=1
-  w=matrix(gamma[w],n,n)
-  w[upper.tri(w)==0]=0
-  alpha=0.05
-  
+for (q in c(0.6, 0.7, 0.8)) {
+  ### Initialise Hyperparameters
+  gamma <- q^(1:n) * (1 - q) / q
+  tau <- 0.8
+  lambda <- 0.16
+  w <- abs(matrix(1:n - 1, nrow = n, ncol = n, byrow = TRUE) - (1:n - 1))
+  w[w == 0] <- 1
+  w <- matrix(gamma[w], n, n)
+  w[upper.tri(w) == 0] <- 0
+  alpha <- 0.05
 
-##ADDIS-Spending
-alpha_ind_Spending[,count]=ADDIS_Spending(alpha, gamma, tau,lambda,lags, p, n)
-rej_Spending[,count]=alpha_ind_Spending[,count]>=p
-n_rej_Spending[count]=sum(rej_Spending[,count])
 
-##Closed ADDIS-Spending
-alpha_ind_closed_ADDIS[,count]=closed_ADDIS_Spending(alpha, gamma, tau,lambda,lags, p, n)
-rej_closed_ADDIS[,count]=alpha_ind_closed_ADDIS[,count]>=p
-n_rej_closed_ADDIS[count]=sum(rej_closed_ADDIS[,count])
+  ## ADDIS-Spending
+  alpha_ind_Spending[, count] <- ADDIS_Spending(alpha, gamma, tau, lambda, lags, p, n)
+  rej_Spending[, count] <- alpha_ind_Spending[, count] >= p
+  n_rej_Spending[count] <- sum(rej_Spending[, count])
 
-##Online-Graph
-alpha_ind_Graph_online[,count]=Online_Graph(alpha, gamma, w, p, n)
-rej_Graph_online[,count]=alpha_ind_Graph_online[,count]>=p
-n_rej_Graph_online[count]=sum(rej_Graph_online[,count])
+  ## Closed ADDIS-Spending
+  alpha_ind_closed_ADDIS[, count] <- closed_ADDIS_Spending(alpha, gamma, tau, lambda, lags, p, n)
+  rej_closed_ADDIS[, count] <- alpha_ind_closed_ADDIS[, count] >= p
+  n_rej_closed_ADDIS[count] <- sum(rej_closed_ADDIS[, count])
 
-#Closed Alpha-Spending
-alpha_ind_closed_Alpha[,count]=closed_Alpha_Spending(alpha, gamma, p, n)
-rej_closed_Alpha[,count]=alpha_ind_closed_Alpha[,count]>=p
-n_rej_closed_Alpha[count]=sum(rej_closed_Alpha[,count])
+  ## Online-Graph
+  alpha_ind_Graph_online[, count] <- Online_Graph(alpha, gamma, w, p, n)
+  rej_Graph_online[, count] <- alpha_ind_Graph_online[, count] >= p
+  n_rej_Graph_online[count] <- sum(rej_Graph_online[, count])
 
-#Alpha-Spending
-alpha_ind_Alpha[,count]=alpha_Spending(alpha, gamma, p, n)
-rej_Alpha[,count]=alpha_ind_Alpha[,count]>=p
-n_rej_Alpha[count]=sum(rej_Alpha[,count])
+  # Closed Alpha-Spending
+  alpha_ind_closed_Alpha[, count] <- closed_Alpha_Spending(alpha, gamma, p, n)
+  rej_closed_Alpha[, count] <- alpha_ind_closed_Alpha[, count] >= p
+  n_rej_closed_Alpha[count] <- sum(rej_closed_Alpha[, count])
 
-count=count+1
+  # Alpha-Spending
+  alpha_ind_Alpha[, count] <- alpha_Spending(alpha, gamma, p, n)
+  rej_Alpha[, count] <- alpha_ind_Alpha[, count] >= p
+  n_rej_Alpha[count] <- sum(rej_Alpha[, count])
+
+  count <- count + 1
 }
-
-
-
